66179e09f9431aa01077f3458b1d3f4115046bea
Pedro Santos FIXO <pedro-santos@hotmail.com>
2014-05-26 23:06:28 +0100

    - State machine for several modes. - Indicate dimensions of the board. - Indicate the position of beginning and ending of lines. - Add additional auxiliary functions.

--- AuxFun.py
+++ AuxFun.py
@@ -31,4 +31,87 @@ def pressOK(posClick):
         return True;
     
     return False;
+
+def changeMode(posClick, actMode):
     
+    valX = posClick[0];
+    valY = posClick[1];
+    
+    if actMode == 0 or actMode == 1:
+        if valX < 90 + 80 and valX > 90 and valY < 10 + 50 and valY > 10:
+            return 0;
+        if valX < 230 + 80 and valX > 230 and valY < 10 + 50 and valY > 10:
+            return 1;
+    
+    return actMode;
+
+def estimateDim(linNum1, linNum2, colNum1, colNum2):
+    dimX = 5;
+    dimY = 5;
+    
+    if linNum2 < 0 and linNum1 >= 0:
+        dimY = linNum1;
+        
+    if linNum2 >= 0 and linNum1 >= 0:
+        dimY = 10*linNum1 + linNum2;
+        
+    if colNum2 < 0 and colNum1 >= 0:
+        dimX = colNum1;
+        
+    if colNum2 >= 0 and colNum1 >= 0:
+        dimX = 10*colNum1 + colNum2;
+    
+    return [dimX, dimY];
+
+def generate2DMatrix(dim):
+    
+    a = [];
+    for i in xrange(dim[1]):
+        a.append([])
+        for j in xrange(dim[0]):
+            a[i].append(0)
+            
+    return a;
+
+def estimatePosIJ(dim, posClick):
+    
+    val = [-1, -1];
+    
+    for i in range(dim[1]):
+        for j in range(dim[0]):
+            if posClick[0] > j*100 + 100 and posClick[0] < j*100 + 100 + 100 and posClick[1] > i*100 + 100 and posClick[1] < i*100 + 100 + 100:
+                val = [i, j];
+            
+    return val;
+
+def estimateColor(valIn):
+    
+    val = valIn;
+    
+    Red = 0;
+    Green = 0;
+    Blue = 0;
+    
+    for i in range(1, 6):
+        if i == val:
+            Red = i*51;
+            return [Red, Green, Blue];
+        
+    val = val - 5;
+    for i in range(1, 6):
+        if i == val:
+            Red = 255;
+            Green = i*51;
+            return [Red, Green, Blue];
+        
+    val = val - 5;
+    for i in range(1, 6):
+        if i == val:
+            Red = 255;
+            Green = 255;
+            Blue = i*255;
+            return [Red, Green, Blue];
+    
+    color = [Red, Green, Blue];
+    
+    return color;
--- main.py
+++ main.py
@@ -18,6 +18,16 @@ size = [370, 450]
 screen = pygame.display.set_mode(size)
 
 step = 0;
+linNum1 = -1;
+linNum2 = -1;
+
+colNum1 = -1;
+colNum2 = -1;
+
+initPoints = [];
+
+indexCount = 0;
+newPoint = True;
  
 pygame.display.set_caption("Solver Connect Gui")
  
@@ -30,22 +40,106 @@ while not done:
     # This limits the while loop to a max of 10 times per second.
     # Leave this out and we will use all CPU we can.
     clock.tick(10)
-     
+    
+    numPress = -1;
+    
     for event in pygame.event.get(): # User did something
         if event.type == pygame.MOUSEBUTTONUP:
+            
             pos = pygame.mouse.get_pos()
             print pos;
-            print estimateButton(pos);
-            print pressOK(pos);
+            
+            if step == 2:
+                dimTab = estimateDim(linNum1, linNum2, colNum1, colNum2);
+                indNewPoint = estimatePosIJ(dimTab, pos);
+                
+                if indNewPoint[0] >= 0:
+                    if newPoint:
+                        indexCount = indexCount + 1;
+                        newPoint = False;
+                    else:
+                        newPoint = True;
+                    initPoints[indNewPoint[0]][indNewPoint[1]] = indexCount;
+
+            elif step == 0 or step == 1:
+                numPress = estimateButton(pos)
+                print numPress;
+                print pressOK(pos);
+                step = changeMode(pos, step)
+                print estimateDim(linNum1, linNum2, colNum1, colNum2);
+            
+                if pressOK(pos):
+                    step = 2;
+                    dimTab = estimateDim(linNum1, linNum2, colNum1, colNum2);
+                    size = [100*dimTab[0] + 200, 100*dimTab[1] + 200];
+                    screen = pygame.display.set_mode(size)
+                    initPoints = generate2DMatrix(dimTab);
+            
         if event.type == pygame.QUIT: # If user clicked close
             done=True # Flag that we are done so we exit this loop
      
     # Clear the screen and set the screen background
     screen.fill(WHITE)
 
-    if step == 0 or step == 1 or step == 2:
+    if step == 0 or step == 1:
+        
+        if numPress >= 0:
+            if step == 0:
+                if linNum1 < 0:
+                    linNum1 = numPress;
+                elif linNum2 < 0:
+                    linNum2 = numPress;
+            if step == 1:
+                if colNum1 < 0:
+                    colNum1 = numPress;
+                elif colNum2 < 0:
+                    colNum2 = numPress;
         
         basicfont = pygame.font.SysFont(None, 50)
+        
+        if step == 0:
+            pygame.draw.rect(screen, BLUE, [90, 10, 80, 50], 2)
+        else:
+            pygame.draw.rect(screen, BLACK, [90, 10, 80, 50], 2)
+            
+        if linNum1 >= 0:
+            text = basicfont.render(str(linNum1), True, (0, 0, 0), (255, 255, 255))
+            textrect = text.get_rect()
+            textrect.centerx = 120;
+            textrect.centery = 30;
+            screen.blit(text, textrect)
+            
+        if linNum2 >= 0:
+            text = basicfont.render(str(linNum2), True, (0, 0, 0), (255, 255, 255))
+            textrect = text.get_rect()
+            textrect.centerx = 140;
+            textrect.centery = 30;
+            screen.blit(text, textrect)
+        
+        if colNum1 >= 0:
+            text = basicfont.render(str(colNum1), True, (0, 0, 0), (255, 255, 255))
+            textrect = text.get_rect()
+            textrect.centerx = 260;
+            textrect.centery = 30;
+            screen.blit(text, textrect)
+            
+        if colNum2 >= 0:
+            text = basicfont.render(str(colNum2), True, (0, 0, 0), (255, 255, 255))
+            textrect = text.get_rect()
+            textrect.centerx = 280;
+            textrect.centery = 30;
+            screen.blit(text, textrect)
+        
+        text = basicfont.render('X', True, (0, 0, 0), (255, 255, 255))
+        textrect = text.get_rect()
+        textrect.centerx = 200;
+        textrect.centery = 40;
+        screen.blit(text, textrect)
+        
+        if step == 1:
+            pygame.draw.rect(screen, BLUE, [230, 10, 80, 50], 2)
+        else:
+            pygame.draw.rect(screen, BLACK, [230, 10, 80, 50], 2)
         
         num = 1;
         
@@ -74,6 +168,23 @@ while not done:
         textrect = text.get_rect()
         textrect.centerx = 105 + 120/2.0;
         textrect.centery = 370 + 70/2.0;
+        screen.blit(text, textrect)
+    
+    if step == 2:
+        dimTab = estimateDim(linNum1, linNum2, colNum1, colNum2);
+        
+        for i in range(dimTab[1]):
+            for j in range(dimTab[0]):
+                pygame.draw.rect(screen, BLACK, [j*100 + 100, i*100 + 100, 100, 100], 2)
+                if initPoints[i][j] > 0:
+                    pygame.draw.circle(screen, estimateColor(initPoints[i][j]), [j*100 + 100 + 50, i*100 + 100 + 50], 40)
+                    
+        pygame.draw.rect(screen, BLACK, [round(dimTab[0]/2)*100 + 100, 10, 100, 70], 2)
+        basicfont = pygame.font.SysFont(None, 80)
+        text = basicfont.render('OK', True, (255, 0, 0), (255, 255, 255))
+        textrect = text.get_rect()
+        textrect.centerx = round(dimTab[0]/2)*100 + 150;
+        textrect.centery = 50;
         screen.blit(text, textrect)
     
     # This MUST happen after all the other drawing commands.
